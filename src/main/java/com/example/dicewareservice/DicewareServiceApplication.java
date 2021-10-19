54ca8fbd604b43f1b66931157be7c2341b75ff79
Royce Reano <reano.royce@gmail.com>
2021-10-19 14:24:50 -0600

    Initial commit

---
+++ src/main/java/com/example/dicewareservice/DicewareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dicewareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicewareServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DicewareServiceApplication.class, args);
+  }
+
+}
