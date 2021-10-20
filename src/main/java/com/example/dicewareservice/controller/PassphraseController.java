1f0c036f4a931f5d466c9eb26834964b062d473c
Royce Reano <reano.royce@gmail.com>
2021-10-20 11:43:55 -0600

    Generated passphrase.

--- src/main/java/com/example/dicewareservice/controller/PassphraseController.java
+++ src/main/java/com/example/dicewareservice/controller/PassphraseController.java
@@ -1,8 +1,10 @@
 package com.example.dicewareservice.controller;
 
 import com.example.dicewareservice.service.PassphraseGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,13 +13,15 @@ public class PassphraseController {
 
   private final PassphraseGenerator generator;
 
-  public PassphraseController(PassphraseGenerator generator) {
+  @Autowired
+  public PassphraseController(
+      PassphraseGenerator generator) {
     this.generator = generator;
   }
 
 
   @GetMapping("/transient")
-  public String[] generate(int length) {
+  public String[] generate(@RequestParam(defaultValue = "5") int length) {
     return generator.generate(length);
 
   }
